1ac5bdefd83a72f0232c7db715a58591b5c454ed
Russell Bentulan <russellbentulan@gmail.com>
2019-11-13 13:59:13 -0500

    added video examples 📷📷📷

--- particlesjs/js/slide-1.js
+++ particlesjs/js/slide-1.js
@@ -120,14 +120,6 @@ particlesJS(
         }
       }
     },
-    retina_detect: true,
-    config_demo: {
-      hide_card: false,
-      background_color: "#b61924",
-      background_image: "",
-      background_position: "50% 50%",
-      background_repeat: "no-repeat",
-      background_size: "cover"
-    }
+    retina_detect: true
   }
 );
