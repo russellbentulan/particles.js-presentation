053c6dda5c05b1f219fcefc468069f2f514646f6
Russell Bentulan <russellbentulan@gmail.com>
2019-11-13 14:22:07 -0500

    added the snow example ❄🌨⛄

--- particlesjs/js/slide-2.js
+++ particlesjs/js/slide-2.js
@@ -61,62 +61,17 @@ particlesJS(
           sync: false
         }
       },
-      line_linked: {
-        enable: true,
-        distance: 150,
-        color: "#ffffff",
-        opacity: 0.4,
-        width: 1
-      },
       move: {
         enable: true,
         speed: 20,
         direction: "none",
-        random: false,
+        random: true,
         straight: false,
         out_mode: "out",
         attract: {
           enable: false,
           rotateX: 600,
           rotateY: 1200
-        }
-      }
-    },
-    interactivity: {
-      detect_on: "canvas",
-      events: {
-        onhover: {
-          enable: true,
-          mode: "repulse"
-        },
-        onclick: {
-          enable: true,
-          mode: "push"
-        },
-        resize: true
-      },
-      modes: {
-        grab: {
-          distance: 400,
-          line_linked: {
-            opacity: 1
-          }
-        },
-        bubble: {
-          distance: 400,
-          size: 40,
-          duration: 2,
-          opacity: 8,
-          speed: 3
-        },
-        repulse: {
-          distance: 200
-        },
-        push: {
-          particles_nb: 4
-        },
-        remove: {
-          particles_nb: 2
         }
       }
     },
